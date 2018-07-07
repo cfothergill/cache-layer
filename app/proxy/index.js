ea6eb08fafbcf5b7fc2f991ae068742abcd4e073
Chris Fothergill <chris@fothergill.me>
2018-07-07 12:22:10 +0100

    Extract retry and fallback logic

--- app/proxy/index.js
+++ app/proxy/index.js
@@ -1,34 +1,19 @@
 // @flow
 
 import type { ExpressRoute, Cache } from '../types';
-import forward from './forward';
-import type { Request } from './forward';
+import createRunner from './runner';
 
 export default (host: string, cache: Cache, maxRetries: number): ExpressRoute => {
-  const proxy = async (req: Request, retries: number) => {
-    const res = await forward(req);
-
-    if (res.status > 399) {
-      if (cache.has(req.url)) {
-        return cache.get(req.url);
-      }
-
-      if (retries > 0) {
-        return await proxy(req, retries - 1);
-      }
-    }
-
-    return res;
-  };
-
   return (req, res, next) => {
     (async () => {
       try {
         const url = `https://${host}/${req.path}`;
-        const { status, headers, body } = await proxy({
+        const runner = createRunner(cache, maxRetries);
+
+        const { status, headers, body } = await runner({
           url: url,
           headers: req.headers,
-        }, maxRetries);
+        });
 
         res.status(status).set(headers).send(body);
         await cache.set(url, { status, headers, body });
---
+++ app/proxy/runner.js
@@ -0,0 +1,25 @@
+// @flow
+
+import type { Cache } from '../types';
+import forward from './forward';
+import type { Request } from './forward';
+
+export default (cache: Cache, maxRetries: number) => {
+  const run = async (req: Request, retries: number) => {
+    const res = await forward(req);
+
+    if (res.status > 399) {
+      if (cache.has(req.url)) {
+        return cache.get(req.url);
+      }
+
+      if (retries > 0) {
+        return await run(req, retries - 1);
+      }
+    }
+
+    return res;
+  };
+
+  return (req: Request) => run(req, maxRetries);
+};
